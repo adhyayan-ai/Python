88317570ae7473d666292ae32edce7a95e32e3ab
adhyayan-ai <69051467+adhyayan-ai@users.noreply.github.com>
2020-09-05 11:49:16 -0700

    Add files via upload
    The first one involved list slicing, but the second one is without slicing AND without the reverse() function. On the second one, I used while loops, if statements, and the len() function to get the length of the input.

---
+++ palindrome_fixed_len.py
@@ -0,0 +1,17 @@
+w = input('Please enter your first name here: ')
+if w == "quit":
+    quit()
+w = w.upper()
+count = 1
+condition = (len(w) / 2)
+while count < condition:
+    if w[count - 1] == w[count - (count * 2)]:
+        pass
+    elif w[count - 1] != w[count - (count * 2)]:
+        print("Not a palindrome.")
+        quit()
+    else:
+        print("Something else went wrong. Please contact the developers. ")
+        quit()
+    count += 1
+print("Palindrome!")
