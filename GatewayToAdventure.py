bad7a8d3e3ae2c7483e26bd3736a4f90efe8e62c
adhyayan-ai <69051467+adhyayan-ai@users.noreply.github.com>
2020-10-24 23:41:13 -0700

    Add files via upload
    v 1.5

--- GatewayToAdventure.py
+++ GatewayToAdventure.py
@@ -57,11 +57,13 @@ class GameWorld:
         return questionsRight
 
     def survivalQuizMethod(self, questionAndAnswer):
-        questionsRight = 0
+        questionsRight = 1
         for question in questionAndAnswer:
             answer = input("Enter the letter of your answer: " + question[0] + "\n: ")
             if answer == question[1]:
-                questionsRight += 1
+                questionsRight *= 1
+            elif answer != question[1]:
+                questionsRight *= 0
             elif answer.capitalize() == "Quit":
                 # User wants to Quit the Game now
                 print(f"User wants to quit the game now")
@@ -83,13 +85,24 @@ class GameWorld:
 
 class Nixoterra(GameWorld):
     #name = "Nixoterra"
-    qNADictQ = {'testnq': 'ansnq'}
-    qNADictS1 = {'testns1': 'ansns1'}
-    qNADictS2 = {'testns2': 'ansns2'}
+    qNADictQ = {
+    "What is the freezing point of snow?\n a. 0 degrees celcius\n b. 30 degrees celcius\n c. 100 degrees celcius\n d. -5 degrees celcius": "d",
+    "Which ball do we use in the snow ball activity at snow world?\n a. Leather Ball\n b. Foot Ball\n c. Snow Ball\n d. All of the above": "d",
+    "Which cartoon character does snow world has?\n a. Penguin\n b. Olaf\n c. Snowman\n d. All of the above": "d"
+    }
+    qNADictS1 = {
+    "Describe a typical workweek for a snowboard instructor\n a. Sleep all week long\n b. Play with Ice\n c. Instruct and provide training to groups/individuals": "c",
+    "what's your level of experience with edging, waxing and mounting skis and snowboard?\n a. Novice\n b. Proficient\n c. expert": "c"
+    }
+    
+    qNADictS2 = {
+    "What equipment is/are required for working as an experienced snowmaker\n a. Snow guns\n b. hoses\n c. hydrants\n d. All of the above": "d",
+    "Can you safely lift 50 lbs\n a. Yes\n b. No": "a" 
+    }    
     def __init__(self):
         self.worldName = "Nixoterra"
         self.worldType = "SnowWorld"
-        self.essentials = ["Plough", "Car"]
+        self.essentials = ["Mittens", "Snowboots", "Ski Goggles", "Wool Socks", "Hand warmer"]
         #ToDo: Fill real name of cpital city
         self.capitalCity = "cNTerra"
         self.qualifyingQuiz = QualifyingQuiz(Nixoterra.qNADictQ)
@@ -98,13 +111,24 @@ class Nixoterra(GameWorld):
     
 class Silvia(GameWorld):
     #name = "Silvia"
-    qNADictQ = {'testsq': 'anssq'}
-    qNADictS1 = {'testss1': 'ansss1'}
-    qNADictS2 = {'testss2': 'ansss2'}
+    qNADictQ = {
+    "Can you use a GPS device effectively?\n a. Yes\n b. No": "a",
+    "Which of the following statement is true?\n a. Forest is on land\n; b. Forest is under water": "a",
+    "What not to do in a Forest world?\n a. Start fire\n b. Climb on Tree\n c. Enjoy Nature": "a"
+    }
+    qNADictS1 = {
+    "Name one of the heavy equipments used in the forest operations\n a. Dump trucks\n b. Toycar\n c. Teddybear": "a",
+    "Which is NOT one of the 4 forest types?\n a. Temperate\n b. Tropical\n c. Subtropical\n d. Arboreal" : "d"
+    }
+    qNADictS2 = {
+    "Forest soil is a natural water filter\n a. True\n b. False": "a",
+    "What is the definition of a forest?\n a. A public green area in a town, used for recreation.\n b. An area rich in biodiversity\n c. A large area of land densely populated by trees\n d. A flat area covered in plants" : "c"
+    }
+
     def __init__(self):
         self.worldName = "Silvia"
         self.worldType = "ForestWorld"
-        self.essentials = ["axe", "fire"]
+        self.essentials = ["Mosquito Repellent", "Backpack", "Poncho", "Hiking boots", "Trail shoes"]
         self.capitalCity = "cSilvia"
         self.qualifyingQuiz = QualifyingQuiz(Silvia.qNADictQ)
         self.survivalQuiz1 = SurvivalQuiz(Silvia.qNADictS1)
@@ -112,28 +136,50 @@ class Silvia(GameWorld):
 
 class Aquamundi(GameWorld):
     #name = "Aquamundi"
-    qNADictQ = {'testaq': 'ansaq'}
-    qNADictS1 = {'testas1': 'ansas1'}
-    qNADictS2 = {'testas2': 'ansas2'}
+    qNADictQ = {
+    "Spell Water\n a. Water\n b. Waiter": "a",
+    "Is water same as wind?\n a.Yes\n b. No": "b",
+    "Do you know how to swim?\n a. Yes\n b. No": "a"
+    }
+    qNADictS1 = {
+    "Why do you want to be a Dive instructor?\n a. I like the word Instructor\n b. I love teaching diving": "b",
+    "Do you have a Captain's license?\n a. Yes\n b. No": "a"
+    }
+    qNADictS2 = {
+    "In case of any emergency call, what will be your first step?\n a. Ignore\n b. Immediately act on it" : "b",
+    "What is a large natural stream of flowing water that ends in a sea?\n a. Lake\n b. Ocean\n c. Glacier\n d. River" : "d"
+    }
+
     def __init__(self):
         self.worldName = "Aquamundi"
         self.worldType = "WaterWorld"
         self.capitalCity = "cAMundi"
-        self.essentials = ["FishHunt", "LifeJacket", "snorkelKit"]
+        self.essentials = ["Swimcap", "Bandana", "Float suit", "Rash guard", "Alternate Air Source"]
         self.qualifyingQuiz = QualifyingQuiz(Aquamundi.qNADictQ)
         self.survivalQuiz1 = SurvivalQuiz(Aquamundi.qNADictS1)
         self.survivalQuiz2 = SurvivalQuiz(Aquamundi.qNADictS2)
 
 class Montelocus(GameWorld):
     #name = "Montelocus"
-    qNADictQ = {'testmq': 'ansmq'}
-    qNADictS1 = {'testms1': 'ansms1'}
-    qNADictS2 = {'testms2': 'ansms2'}
+    qNADictQ = {
+    "Do you know how to use mountain gear?\n a. Yes\n b. No": "a",
+    "Are you a wayfinder or a lost sheep?\n a. Wayfinder\n b. Lostsheep": "a",
+    "Are you afraid of heights?\n a. Yes\n b. No": "b"
+    }
+    qNADictS1 = {
+    "What would you select to climb in tricky mountain conditions?\n a. Mountaineering boots\n  b. mountains": "a",
+    "What would you do if someone calls for help?\n a. It's their problem, I will just ignore\n b. Go and help them": "b"
+    }
+
+    qNADictS2 = {
+    "Which of the following is the highest part of a mountain?\n a. Peak\n  b. Base\n c. Slope\n d. None of the above": "a",
+    "What should you do if you fall off a mountain during Climbing?\n a. Have a quick snack\n b. Make a quick phone call\n c. Take a selfie and post it on instagram\n d. Use a harness": "d"
+    }
     def __init__(self):
         self.worldName = "Montelocus"
         self.worldType = "MountainWorld"
         self.capitalCity = "cMlocus"
-        self.essentials = ["rope", "harness"]
+        self.essentials = ["Climbing Helmet", "Ropes", "Harness", "Mountaineering Boots", "Crampons"]
         self.qualifyingQuiz = QualifyingQuiz(Montelocus.qNADictQ)
         self.survivalQuiz1 = SurvivalQuiz(Montelocus.qNADictS1)
         self.survivalQuiz2 = SurvivalQuiz(Montelocus.qNADictS2)
@@ -176,14 +222,16 @@ class User:
     def getGems(self):
         return self.gems
 
-    def buyEssentials(self):
+    def buyEssentials(self, amount=5):
         ls = self.location.getEssentialList()
-        print(ls)
+        #print("\n".join(ls))
+        outputList = [str(i+1) + ". " + ls[i] for i in range(len(ls))]
+        print("\n".join(outputList))
         sizeEssentialList = len(ls)
         essentialsList = []
 
-        print(f"Currently, Player has {', '.join(self.assets)} in his essentialList")
-        choices = input("Input your selection as numbers 1, 2, 3, input your choices separated by comma: ")
+        #print(f"Currently, Player has {', '.join(self.assets)} in his essentialList")
+        choices = input("Input your selection as numbers 1, 2, 3, 4, or 5 separated by comma: ")
         if choices.capitalize() == "Quit":
             #print("Quit")
             # User input "Quit" at this stage. So, just quit the game.
@@ -205,14 +253,13 @@ class User:
             return essentialsList
 
         for j in choices:
-            if self.coins >= 20:
+            if self.spendCoin(amount):
                 essentialsList.append(ls[j-1])
-                self.spendCoin(amount=20)
             else:
                 print(f"You don't have enough money to buy {j}. You only have {self.coins} coins left.")
                 break
         self.assets = essentialsList
-        print(f"Currently, Player has {', '.join(self.assets)} in his essentialList")
+        print(f"Currently, Player has {', '.join(self.assets)} in their essentialList")
 
         return self.assets
 
@@ -227,7 +274,7 @@ class User:
         else: 
             # Wrong argument, should not have reached here. Return anyway!
             userSurvival = "Quit"
-        survivalResults = self.location.qualifyingQuizMethod(userSurvival)
+        survivalResults = self.location.survivalQuizMethod(userSurvival)
         return survivalResults
 
     def navigateToCapitalcity(self, amount=20):
@@ -261,6 +308,7 @@ class GameSystem:
         self.buyGemAmount = 25
         self.minEssentials = 3
         self.maxAssetNum = 5
+        self.survivalWinCoins = 10
 
     def gameStart(self):        
         #if userLocation.capitalize() in ["Nixoterra", "Silvia", "Aquamundi", "Montelocus"]:
@@ -311,7 +359,7 @@ class GameSystem:
             self.gameQuit(userName)
             return False
 
-        if not qualifyingResults:
+        if qualifyingResults < self.minCorrectAnswers:
             # User failed the qualifying quiz
             # Exit the game already :)
             print(f"{bcolors.FAIL}You failed. {bcolors.ENDC}")
@@ -330,7 +378,7 @@ class GameSystem:
         essentialsList = player.buyEssentials()
 
         if not essentialsList:
-            # No item selected from essential list
+            # No item selected from the essential list
             # Player can't survive in the world
             # Exit the world with message
             print(f"{bcolors.FAIL}Quitting as {userName} didn't select any essential items to enter the new world.{bcolors.ENDC}")
@@ -341,7 +389,13 @@ class GameSystem:
             self.gameQuit(userName)
             return gameReturn
 
-        essentialsList = ", ".join(essentialsList)
+        #essentialsList = ", ".join(essentialsList)
+        print(f"{userName} Assets: ", player.getAssets())
+        if len(player.getAssets()) < self.minEssentials:
+            print(f"{bcolors.FAIL}You need to select at least {self.minEssentials} from the list.{bcolors.ENDC}")
+            self.gameQuit(userName)
+            return gameReturn
+
         print(f"Player current coins: {player.getCoins()}")
         #print(essentialsList)
 
@@ -349,13 +403,13 @@ class GameSystem:
             print(f"{userName} just bought one gem as they enter the world of {world.worldName}")
         
         gameReturn = True
-        
         survival1Results = player.takeSurvivalQuiz('survivalQuiz1')
 
         if str(survival1Results).capitalize() == "Quit":
             self.gameQuit(userName)
             # User entered Quit exit the loop and return accordingly
             gameReturn = False
+            return gameReturn
 
         if not survival1Results:
             print("You failed. ")
@@ -370,14 +424,12 @@ class GameSystem:
                 if not survival1Results:
                     print("You failed again. ")
                 else:
-                    player.earnCoin(10 * survival1Results)
-                    if survival1Results >= self.minCorrectAnswers:
-                        player.collectGem()
+                    player.earnCoin(self.survivalWinCoins * survival1Results)
+                    player.collectGem()
                     print(f"Player current coins: {player.getCoins()} current Gems:{player.getGems()}")        
         else:
-            player.earnCoin(10 * survival1Results)
-            if survival1Results >= self.minCorrectAnswers:
-                player.collectGem()
+            player.earnCoin(self.survivalWinCoins * survival1Results)
+            player.collectGem()
             print(f"Player current coins: {player.getCoins()} current Gems:{player.getGems()}")
         
         survival2Results = player.takeSurvivalQuiz('survivalQuiz2')
@@ -386,6 +438,7 @@ class GameSystem:
             self.gameQuit(userName)
             # User entered Quit exit the loop and return accordingly
             gameReturn = False
+            return gameReturn
 
         if not survival2Results:
             print("You failed. ")
@@ -396,22 +449,22 @@ class GameSystem:
                     self.gameQuit(userName)
                     # User entered Quit exit the loop and return accordingly
                     gameReturn = False
+                    return gameReturn
 
                 if not survival2Results:
                     print("You failed again. ")
                 else:
-                    player.earnCoin(10 * survival2Results)
-                    if survival2Results >= self.minCorrectAnswers:
-                        player.collectGem()
+                    player.earnCoin(self.survivalWinCoins * survival2Results)
+                    player.collectGem()
                     print(f"Player current coins: {player.getCoins()} current Gems:{player.getGems()}")        
         else:
-            player.earnCoin(10 * survival2Results)
-            if survival2Results >= self.minCorrectAnswers:
-                player.collectGem()
+            player.earnCoin(self.survivalWinCoins * survival2Results)
+            player.collectGem()
             print(f"Player current coins: {player.getCoins()} current Gems:{player.getGems()}")
 
         # Navigate to Capital City  
         if player.getGems() >= self.minGemCapitalCity:
+            print(f"{userName} has reached the capital city of {userLocation}")
             if len(player.getAssets()) == self.maxAssetNum :
                 player.navigateToCapitalcity(10)
             else:
@@ -432,11 +485,16 @@ class GameSystem:
         if player.getGems() >= self.minGems:
             # Check if Player has enough coins to win the game
             if player.getCoins() >= self.minCoins:
+                print(f"player {userName} has {player.getCoins()} coins and {player.getGems()} gems!")
                 print(f"Player {userName} Won!!")
             else:
-                print(f"You need {self.minCoins} to win the game!")
+                print(f"Player {userName} lost!!")
+                print(f"Player {userName} has {player.getCoins()}")
+                print(f"You needed {self.minCoins} coins to win the game!")
         else:
-            print(f"You need {self.minGems} to win the game!")
+            print(f"Player {userName} lost!!")
+            print(f"Player {userName} has {player.getCoins()}")
+            print(f"You needed {self.minGems} gems to win the game!")
                 
         return gameReturn
 
