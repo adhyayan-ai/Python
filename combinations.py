0709b64b279860ebfc4013da6d31cbf8fa9c789e
adhyayan-ai <69051467+adhyayan-ai@users.noreply.github.com>
2020-11-05 14:09:05 -0800

    Add files via upload

--- combinations.py
+++ combinations.py
@@ -1,19 +1,14 @@
-def factorial(num):
-    if num == 1:
-        return 1
-    return num * factorial(num-1)
-
-if factorial(4) != 24:
-    exit()   
-
-n = int(input("Enter the number of items you want to choose from: "))
-r = int(input("Enter the number items you want to choose: "))
-
-if n < r:
-    print("The number of items you want to choose from must be equal to or more than the number of items you are choosing. Please try again. ")
-    exit()
-
 def combinations(n, r):
+    def factorial(num):
+        if num == 1:
+            return 1
+        return num * factorial(num-1)
+
+    n = int(input("Enter the number of items you want to choose from: "))
+    r = int(input("Enter the number items you want to choose: "))
+    if n < r:
+        print("The number of items you want to choose from must be equal to or more than the number of items you are choosing. Please try again. ")
+        exit()
     return factorial(n)/(factorial(r)*factorial(n-r))
 
-print(combinations(n, r))
+print(int(combinations(n, r)))
